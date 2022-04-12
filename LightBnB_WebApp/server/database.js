7102a2f9f2a684aa861ee6587371c171ca85aee6
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-04-12 15:57:50 -0700

    add property to DB

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -46,6 +46,7 @@ const getUserWithId = function(id) {
   WHERE id = $1;
   `,[id])
   .then((res) => {
+    console.log('🟠ID:',id)
     return res.rows[0];
   })
   .catch((err) => {
@@ -116,11 +117,11 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-  
+
   let queryParams = [];
   let queryString = `SELECT properties.*, AVG(rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id`;
+  LEFT JOIN property_reviews ON properties.id = property_id`;
   
   const havingStatment = [`HAVING`,`AND`];
   let index = 0;
@@ -180,9 +181,18 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+  .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *
+  `,[property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night * 100, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces || 0, property.number_of_bathrooms || 0, property.number_of_bedrooms || 0])
+  .then((res) => {
+    console.log('🟢 Add new property!')
+    // console.log(res.rows[0])
+    return res.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.addProperty = addProperty;
